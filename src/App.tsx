23c1de45aa61078e4f71e03f3a32abde7f6a3208
Elene <elene@Elenes-MacBook-Air.local>
2024-09-24 11:12:42 +0400

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import "./App.css";
-import { Footer } from "./footer-component/Footer.js";
-import { CountriesCard } from "./countries-app/CountriesCard.js";
-import { Header } from "./header-component/Header.js";
-import { HeroSection } from "./hero-component/HeroSection.js";
+import { Footer } from "./components/footer-component/Footer.js";
+import { CountriesCard } from "./components/countries-app/CountriesCard.js";
+import { Header } from "./components/header-component/Header.js";
+import { HeroSection } from "./components/hero-component/HeroSection.js";
 
 function App() {
   return (
--- src/components/header-component/Header.tsx
+++ src/components/header-component/Header.tsx
@@ -4,6 +4,10 @@ export const Header = () => {
   return (
     <div className={styles.header}>
       <h1>WorldFacts</h1>
+      <a href="#">Countries</a>
+      <a href="#">Maps</a>
+      <a href="#">References</a>
+      <a href="About"></a>
     </div>
   );
 };
