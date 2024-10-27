09b41505a72d52ed144cfd6ab22b8f84d68643a5
Elene <ellebuto@gmail.com>
2024-10-27 04:32:25 +0400

    done husky,ESlint and prettier installation, clear my code

--- src/components/header-component/Header.tsx
+++ src/components/header-component/Header.tsx
@@ -6,8 +6,6 @@ const handleActiveNav = ({ isActive }: NavLinkRenderProps) => {
     return isActive ? styles.activeNavItem : '';
 };
 
-const ela = '';
-
 export const Header = () => {
     const { lang } = useParams<string>();
 
